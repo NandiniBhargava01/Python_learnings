8bb774fceaaa9d5fb0d3186c64d4a1c234fe89f9
Nandini Bhargava <nandinibhargava05@gmail.com>
2024-06-05 17:00:44 +0530

    Add files via upload
    Ask for user information and return a statement regarding the user.

---
+++ Hey_You.py
@@ -0,0 +1,18 @@
+#ask user for Name
+
+name=str(input("What is your name? \n"))
+
+#ask user for age
+
+age=str(input("What is your age? \n"))
+
+#ask user for city
+
+city=str(input("Which city do you live in? \n"))
+#ask user what they enjoy
+
+joy=str(input("What do you like ? \n"))
+#output sentence
+
+
+print("User's name is  "+name+", user's is "+age+" years old. \n User live in"+city+" ,user likes to "+joy+".")
